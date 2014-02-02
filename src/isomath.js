f4c8070326f880550ee7fab76bc25243d33e4859
Damon Oehlman <damon.oehlman@gmail.com>
2014-02-03 08:33:31 +1100

    Remove now defunct src directory

--- src/isomath.js
+++
@@ -1,8 +0,0 @@
-//= types/projection
-
-function isomath(ratio, opts) {
-    return new Projection(Math.atan(ratio || 0.5), opts);
-}
-
-// export the projection type
-isomath.Projection = Projection;
