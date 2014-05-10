60879381578764523e2694dbc56a0e27a79412a0
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-10 20:41:52 +1000

    Update axes example to be beefy compatible

---
+++ examples/axes.js
@@ -0,0 +1,37 @@
+var crel = require('crel');
+var isomath = require('..');
+var canvas = crel('canvas', { width: 800, height: 600 });
+var context = canvas.getContext('2d');
+var projection = isomath(0.5, { clamp: true });
+
+// set the origin for the projection
+projection.origin(200, 370);
+
+context.fillStyle = 'white';
+context.fillRect(0, 0, 400, 400);
+
+// draw the x-axis
+context.strokeStyle = '#FF0000';
+context.beginPath();
+context.moveTo.apply(context, projection.project(0, 0, 0));
+context.lineTo.apply(context, projection.project(200, 0, 0));
+context.closePath();
+context.stroke();
+
+// draw the y-axis
+context.strokeStyle = '#00FF00';
+context.beginPath();
+context.moveTo.apply(context, projection.project(0, 0, 0));
+context.lineTo.apply(context, projection.project(0, 200, 0));
+context.closePath();
+context.stroke();
+
+// draw the z-axis
+context.strokeStyle = '#0000FF';
+context.beginPath();
+context.moveTo.apply(context, projection.project(0, 0, 0));
+context.lineTo.apply(context, projection.project(0, 0, 200));
+context.closePath();
+context.stroke();
+
+document.body.appendChild(canvas);
